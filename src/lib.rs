c49cd7da2ce27376f2124028ab1890caefb9c9f9
GiuseppeMuntoni <g.muntoni@consorziometis.it>
2022-11-21 12:09:07 +0100

    modified function names

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub trait SegmentTreeSpec {
     type S: Clone;
     type F: Clone;
-    
+
     /// Require for all a,b,c: op(a, op(b, c)) = op(op(a, b), c)
     fn op(a: &Self::S, b: &Self::S) -> Self::S;
     /// Require for all a: op(a, identity()) = op(identity(), a) = a
@@ -27,12 +27,16 @@ impl<T: SegmentTreeSpec> SegmentTree<T> {
         tree_val.extend_from_slice(&val);
 
         for i in (1..size).rev() {
-            tree_val[i] = T::op(&tree_val[2*i], &tree_val[2*i+1]);
+            tree_val[i] = T::op(&tree_val[2 * i], &tree_val[2 * i + 1]);
         }
 
         let app = vec![None; size];
 
-        Self{size, val:tree_val, app}
+        Self {
+            size,
+            val: tree_val,
+            app,
+        }
     }
 
     fn modify_node_lazily(&mut self, node_idx: usize, f: &T::F, segment_size: i64) {
@@ -46,23 +50,31 @@ impl<T: SegmentTreeSpec> SegmentTree<T> {
         }
     }
 
-    fn sift_up(&mut self, mut node_idx: usize, segment_size: i64) {
+    fn update_upward(&mut self, mut node_idx: usize, segment_size: i64) {
         while node_idx > 1 {
-            node_idx = node_idx/2;
+            node_idx /= 2;
             match &self.app[node_idx] {
-                Some(f) => self.val[node_idx] = T::apply(&f, &T::op(&self.val[2*node_idx], &self.val[2*node_idx + 1]), segment_size),
-                None => self.val[node_idx] = T::op(&self.val[2*node_idx], &self.val[2*node_idx + 1]),
+                Some(f) => {
+                    self.val[node_idx] = T::apply(
+                        f,
+                        &T::op(&self.val[2 * node_idx], &self.val[2 * node_idx + 1]),
+                        segment_size,
+                    )
+                }
+                None => {
+                    self.val[node_idx] = T::op(&self.val[2 * node_idx], &self.val[2 * node_idx + 1])
+                }
             }
         }
     }
 
-    fn sift_down(&mut self, node_idx: usize) {
+    fn update_downward(&mut self, node_idx: usize) {
         let level = (node_idx as f32).log2().floor() as i32 + 1;
 
         for i in (1..level).rev() {
             let p = node_idx / ((i as f64).exp2() as usize);
             if let Some(ref f) = self.app[p].take() {
-                let s = (i as f64).exp2() as i64; 
+                let s = (i as f64).exp2() as i64;
                 self.modify_node_lazily(p * 2, f, s);
                 self.modify_node_lazily(p * 2 + 1, f, s);
                 self.app[p] = None;
@@ -74,8 +86,8 @@ impl<T: SegmentTreeSpec> SegmentTree<T> {
         l += self.size;
         r += self.size;
 
-        let l0 = l.clone();
-        let r0 = r.clone();
+        let l0 = l;
+        let r0 = r;
         let mut s = 1;
 
         while l < r {
@@ -84,7 +96,7 @@ impl<T: SegmentTreeSpec> SegmentTree<T> {
                 l += 1;
             }
             if r % 2 == 1 {
-                r-=1;
+                r -= 1;
                 self.modify_node_lazily(r, f, s);
             }
 
@@ -93,16 +105,16 @@ impl<T: SegmentTreeSpec> SegmentTree<T> {
             r /= 2;
         }
 
-        self.sift_up(l0, 1);
-        self.sift_up(r0 - 1, 1);
+        self.update_upward(l0, 1);
+        self.update_upward(r0 - 1, 1);
     }
-    
+
     pub fn query(&mut self, mut l: usize, mut r: usize) -> T::S {
         l += self.size;
         r += self.size;
 
-        self.sift_down(l);
-        self.sift_down(r - 1);
+        self.update_downward(l);
+        self.update_downward(r - 1);
 
         let mut result = T::identity();
         while l < r {
@@ -123,8 +135,9 @@ impl<T: SegmentTreeSpec> SegmentTree<T> {
     }
 }
 
+#[cfg(test)]
 mod tests {
-    use crate::{SegmentTreeSpec, SegmentTree};
+    use crate::{SegmentTree, SegmentTreeSpec};
 
     pub enum AssignMax {}
     impl SegmentTreeSpec for AssignMax {
@@ -146,7 +159,8 @@ mod tests {
 
     #[test]
     fn test_eager() {
-        let mut segment_tree:SegmentTree<AssignMax> = SegmentTree::new(vec![2,3,8,4,0,1,3,9]);
+        let mut segment_tree: SegmentTree<AssignMax> =
+            SegmentTree::new(vec![2, 3, 8, 4, 0, 1, 3, 9]);
         segment_tree.update(4, 5, &12);
         assert_eq!(8, segment_tree.query(0, 4));
         assert_eq!(12, segment_tree.query(4, 8));
@@ -154,7 +168,8 @@ mod tests {
 
     #[test]
     fn test_lazy() {
-        let mut segment_tree:SegmentTree<AssignMax> = SegmentTree::new(vec![2,3,8,4,0,1,3,9]);
+        let mut segment_tree: SegmentTree<AssignMax> =
+            SegmentTree::new(vec![2, 3, 8, 4, 0, 1, 3, 9]);
         segment_tree.update(2, 7, &12);
         assert_eq!(20, segment_tree.query(1, 3));
         assert_eq!(15, segment_tree.query(4, 7));
@@ -162,11 +177,11 @@ mod tests {
 
     #[test]
     fn test_lazy_incomplete() {
-        let mut segment_tree:SegmentTree<AssignMax> = SegmentTree::new(vec![2,3,8,4,0,1,3,9,10]);
+        let mut segment_tree: SegmentTree<AssignMax> =
+            SegmentTree::new(vec![2, 3, 8, 4, 0, 1, 3, 9, 10]);
         segment_tree.update(2, 7, &12);
         assert_eq!(20, segment_tree.query(1, 3));
         assert_eq!(15, segment_tree.query(4, 7));
         assert_eq!(10, segment_tree.query(7, 9));
     }
-
 }
